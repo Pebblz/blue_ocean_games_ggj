80cb0de2125a0f6ed19662df586c177153b9556e
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2025-05-10 19:22:04 -0400

    Added Iframes to enemies

--- Assets/Scripts/EnemyScripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyScripts/EnemyHealth.cs
@@ -4,11 +4,14 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField, Tooltip("The Enemy health")] int health = 3;
     [SerializeField, Tooltip("This will add 3DText to the damage of an enemy")] GameObject threeDTextPrefab;
+    [SerializeField] float iFrameCount = .2f;
+    bool canBeHit = true;
     public void GetHit(int damage)
     {
+        if (!canBeHit) return;
         //subtracted health from the amount of damage you do
         health -= damage;
-        if (threeDTextPrefab != null)
+        if ( threeDTextPrefab != null)
         {
             GameObject g = Instantiate(threeDTextPrefab, transform.position + new Vector3(Random.Range(-.2f, .2f), Random.Range(.2f, .5f), Random.Range(-.2f, .2f)),Quaternion.identity);
             g.GetComponent<ThreeDText>().textMeshPro.text = damage.ToString();
@@ -18,5 +21,11 @@ public class EnemyHealth : MonoBehaviour
             //we can change this later to play an animation or make a lootbag when killed
             Destroy(gameObject);
         }
+        canBeHit = false;
+        Invoke("IframesOver", iFrameCount);
+    }
+    void IframesOver()
+    {
+        canBeHit = true;
     }
 }
