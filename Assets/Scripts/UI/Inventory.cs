6a836d3da6745a31c60d889629bf127c7d56045c
jmcgurn <jmcgurn@email.neit.edu>
2025-05-21 00:34:02 -0400

    Added Inventory System First Iteration

---
+++ Assets/Scripts/UI/Inventory.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private Slot[] inventorySlots = new Slot[12];
+    [SerializeField] private Slot[] characterSlots = new Slot[6];
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
