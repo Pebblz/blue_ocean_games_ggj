40334607fccfd0bcdaee659e165800fc1a3e7099
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2025-05-31 16:22:12 -0400

    Level and enemy attack

--- Assets/Scripts/EnemyScripts/ChasingEnemy.cs
+++ Assets/Scripts/EnemyScripts/ChasingEnemy.cs
@@ -5,15 +5,17 @@ public class ChasingEnemy : MonoBehaviour, IEnemy
 {
     private NavMeshAgent agent;
     private Transform target;
+    [SerializeField] Animator anim;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
     private void Update()
     {
-        if (agent != null)
+        if (agent != null )
         {
-            agent.SetDestination(target.position);
+            if(agent.isOnNavMesh)
+                agent.SetDestination(target.position);
         }
     }
     void Attack()
